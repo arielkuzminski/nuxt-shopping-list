d625c6f4e2a5c9f5c22a78e28ea3f83aa4a37610
Ariel Kuźmiński <ariel.kuzminski@gmail.com>
2025-10-12 12:57:00 +0200

    Add Vercel speed insights

--- app/app.vue
+++ app/app.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="bg-gray-800 h-dvh flex justify-center">
+    <SpeedInsights />
+
+    <NuxtLoadingIndicator />
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
   </div>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { SpeedInsights } from "@vercel/speed-insights/nuxt";
+</script>
